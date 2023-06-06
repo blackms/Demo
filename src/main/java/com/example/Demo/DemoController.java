1492b7229a532fce3758324c5d4f5f5651dfd123
Alessio Rocchi <rocchi.b.a@gmail.com>
2023-06-06 09:37:39 +0200

    fixing deploy.yml

--- src/main/java/com/example/Demo/DemoController.java
+++ src/main/java/com/example/Demo/DemoController.java
@@ -1,4 +1,4 @@
-package com.example.Demo.Controllers;
+package com.example.Demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
